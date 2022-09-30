9e9bd569b6324dc082a722eed7e2d8d0d5103af9
hyeleeeeen <heo22969087@gmail.com>
2022-09-30 17:11:35 +0900

    솔로프로젝트

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const header = () => {
+
+    return (
+        <>
+        <header>
+        <div>my todo list</div>
+        </header>
+        </>
+    )
+}
+
+export default header; 
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+
+const Header = () => {
+
+    return (
+        <>
+        <header>
+        <div>my todo list</div>
+        </header>
+        </>
+    )
+}
+
+export default Header; 
